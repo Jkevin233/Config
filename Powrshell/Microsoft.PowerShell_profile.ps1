cc57c26f5713e01a965373ab6fb8fad8413bbb23
Jkevin233 <leomessigoattt@gmail.com>
2023-03-01 10:34:01 +0800

    Add Powershell_config

---
+++ Powrshell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+Import-Module posh-git
+Import-Module Register-Completion
+
+$options = @{
+    HistoryNoDuplicates = $true
+    
+}
+
+Set-PSReadLineOption @options
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+
+Register-Alias touch New-Item
+Register-Alias nrc "nvim C:/Users/JKevin/AppData/Local/nvim/init.lua"
+Register-Alias ncd "cd C:/Users/JKevin/AppData/Local/nvim/"
+Register-Alias dcd "cd C:/Users/JKevin/Documents/Notes/"
+Register-Alias .. "cd .."
+    
+
+Register-Alias gita "git add ."
+Register-Alias gitm "git commit -m"
+Register-Alias gitp "git push"
